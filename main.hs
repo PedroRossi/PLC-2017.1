3f3f3751828aa39d22e0f9db53ac676ffd102af9
Pedro Rossi <pgrr@cin.ufpe.br>
2017-06-29 05:49:30 -0300

    Collisions almost done (spaces shuttles crash, invader got hit, invader reached planet)

--- main.hs
+++ main.hs
@@ -5,7 +5,7 @@ import Graphics.Rendering.OpenGL (GLdouble)
 import Data.Foldable
 
 data GameAttribute = Score Int
-data GateState = Level Int | GameInit Int
+data GameState = Level Int | GameInit Int
 
 type SpaceInvadersObject = GameObject ()
 type SpaceInvadersAction a = IOGame GameAttribute () () () a
@@ -14,6 +14,7 @@ width = 480
 height = 640
 w = fromIntegral width :: GLdouble
 h = fromIntegral height :: GLdouble
+spaceShipSize = 50.0
 
 main :: IO ()
 main = do
@@ -22,7 +23,7 @@ main = do
       gameMap = textureMap 0 1024.0 770.0 w h
       spaceShip = objectGroup "spaceShipGroup" [createSpaceShip]
       spaceShipFire = objectGroup "spaceShipFireGroup" []
-      invader = objectGroup "invaderGroup" [createInvader (fromIntegral (i) :: Double) | i <- [0..10]]
+      invader = objectGroup "invaderGroup" [createInvader (fromIntegral (i) :: Double) | i <- [1..10]]
       invaderFire = objectGroup "invaderFireGroup" []
       initScore = Score 0
       input = [
@@ -36,7 +37,7 @@ main = do
 
 createSpaceShip :: SpaceInvadersObject
 createSpaceShip =
-  let bound = [(0,0),(50,0),(25,50)]
+  let bound = [(0,0),(spaceShipSize,0),(spaceShipSize/2,spaceShipSize)]
       pic = Basic (Polyg bound 1.0 1.0 1.0 Filled)
   in object "spaceShip" pic False (w/2, 20) (0,0) ()
 
@@ -44,21 +45,24 @@ createSpaceShipFire :: GLdouble -> GLdouble -> SpaceInvadersObject
 createSpaceShipFire x y =
   let bound = [(0,0),(1,0),(1,1),(0,1)]
       pic = Basic (Polyg bound 1.0 1.0 1.0 Filled)
-  in object "spaceShipFire" pic False (x+25.0,y) (0,10) ()
+  in object "spaceShipFire" pic False (x+(spaceShipSize/2),y) (0,10) ()
 
-createInvader :: Double -> SpaceInvadersObject
+createInvader :: GLdouble -> SpaceInvadersObject
 createInvader n =
-  let bound = [(0,0),(-25,0),(-12.5,-25)]
-      pic = Basic (Polyg bound 1.0 1.0 1.0 Filled)
-  in object "invader" pic False ((n*30.0),h) (6,-0.2) ()
+  let bound = [(0,0),(-12.5,0),(-7.25,-12.5)]
+      pic = Basic (Polyg bound 255.0 1.0 1.0 Filled)
+  in object "invader" pic False ((n*spaceShipSize),h) (6,-0.2) ()
 
 createInvaderFire :: SpaceInvadersObject
 createInvaderFire = do
   let invaderPic = Basic (Circle 6.0 0.0 1.0 0.0 Filled)
   object "invader" invaderPic False (0,h) (8,-0.2) ()
 
+-- makeItRain :: SpaceInvadersAction ()
+-- makeItRain = do
+--   return
 
-moveSpaceShipToRight :: Modifiers -> Position -> IOGame GameAttribute () () () ()
+moveSpaceShipToRight :: Modifiers -> Position -> SpaceInvadersAction ()
 moveSpaceShipToRight _ _ = do
   obj     <- findObject "spaceShip" "spaceShipGroup"
   (pX,pY) <- getObjectPosition obj
@@ -67,7 +71,7 @@ moveSpaceShipToRight _ _ = do
    then (setObjectPosition ((pX + 5),pY) obj)
    else (setObjectPosition ((w - (sX/2)),pY) obj)
 
-moveSpaceShipToLeft :: Modifiers -> Position -> IOGame GameAttribute () () () ()
+moveSpaceShipToLeft :: Modifiers -> Position -> SpaceInvadersAction ()
 moveSpaceShipToLeft _ _ = do
   obj <- findObject "spaceShip" "spaceShipGroup"
   (pX,pY) <- getObjectPosition obj
@@ -76,7 +80,7 @@ moveSpaceShipToLeft _ _ = do
    then (setObjectPosition ((pX - 5),pY) obj)
    else (setObjectPosition (sX/2,pY) obj)
 
-fireSpaceShip :: Modifiers -> Position -> IOGame GameAttribute () () () ()
+fireSpaceShip :: Modifiers -> Position -> SpaceInvadersAction ()
 fireSpaceShip _ _ = do
   obj <- findObject "spaceShip" "spaceShipGroup"
   (pX,pY) <- getObjectPosition obj
@@ -84,7 +88,12 @@ fireSpaceShip _ _ = do
   addObjectsToGroup [obj] "spaceShipFireGroup"
   drawObject obj
 
-gameCycle :: IOGame GameAttribute () () () ()
+endGame :: SpaceInvadersAction ()
+endGame = do
+  funExit
+  setGameAttribute (Score 0)
+
+gameCycle :: SpaceInvadersAction ()
 gameCycle = do
   (Score n) <- getGameAttribute
   printOnScreen (show n) TimesRoman24 (0,0) 1.0 1.0 1.0
@@ -92,16 +101,24 @@ gameCycle = do
   invaders <- getObjectsFromGroup "invaderGroup"
   spaceShipBullets <- getObjectsFromGroup "spaceShipFireGroup"
   invaderBullets <- getObjectsFromGroup "invaderFireGroup"
+  spaceShip <- findObject "spaceShip" "spaceShipGroup"
 
   forM_ invaders $ \invader -> do
-    col1 <- objectLeftMapCollision invader
-    col2 <- objectRightMapCollision invader
-    when (col1 || col2) (reverseXSpeed invader)
-    col4 <- objectBottomMapCollision invader
-    when col4 $ do
-      funExit
-      setGameAttribute (Score 0)
-
+    wallHit1 <- objectLeftMapCollision invader
+    wallHit2 <- objectRightMapCollision invader
+    when (wallHit1 || wallHit2) (reverseXSpeed invader)
+    invassionSuccess <- objectBottomMapCollision invader
+    crash <- objectsCollision invader spaceShip
+    when (invassionSuccess || crash) $ endGame
+    forM_ invaderBullets $ \b -> do
+      spaceShipHit <- objectsCollision spaceShip b
+      when spaceShipHit $ endGame
+    forM_ spaceShipBullets $ \b -> do
+      invaderHit <- objectsCollision invader b
+      when invaderHit $ do
+        -- atingiu alguem
+        setObjectAsleep True invader
+        setGameAttribute (Score (n+10))
 
 
   showFPS TimesRoman24 (w-40,0) 1.0 0.0 0.0
